6f139120ddd0c0f6aa06cc1326bbe278fe20f159
Rachael-tevis <31253076+Rachael-tevis@users.noreply.github.com>
2017-08-29 12:35:27 -0400

    Add files via upload
    Dear Dr. Kern,
    
    I am very close to creating the final excel document from my yearly averages. However, when data is added to the new file, only the final yearly average is placed into the new excel file. How do I add all of my AVERAGE values into the excel file?

--- Gas_Prices.m
+++ Gas_Prices.m
@@ -1,3 +1,19 @@
-Prices=csvread('EER.csv',3,1) 
+Prices=csvread('EER.csv',3,1); 
+
+size(Prices);
+
+sum(Prices(8:19))
+
+for x= 1:30
+    y = (x*12)-12;
+    
+    AVERAGE = (sum(Prices((8 + y):(19 + y))))/12
+    
+    csvwrite('monthly_average_price.csv',AVERAGE)
+    
+end
+
+  
+    
 
 
