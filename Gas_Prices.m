a5f708be875e87d60d2aecb17db1256c82042217
Jordan Kern <jdkern@live.unc.edu>
2017-08-29 12:44:53 -0400

    Update Gas_Prices.m

--- Gas_Prices.m
+++ Gas_Prices.m
@@ -1,19 +1,15 @@
 Prices=csvread('EER.csv',3,1); 
 
-size(Prices);
-
-sum(Prices(8:19))
-
 for x= 1:30
     y = (x*12)-12;
     
-    AVERAGE = (sum(Prices((8 + y):(19 + y))))/12
-    
-    csvwrite('monthly_average_price.csv',AVERAGE)
-    
+    % add an extra year onto AVERAGE each iteration
+    AVERAGE(x) = (sum(Prices((8 + y):(19 + y))))/12
+       
 end
 
-  
+% put this outside the loop -- I also took the 'transpose' of AVERAGE using the ', to make a column vector
+csvwrite('monthly_average_price.csv',AVERAGE') 
     
 
 
